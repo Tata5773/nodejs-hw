9bde3abe2ea2761d292bdcb1f0d819b795aba5bd
Tata5773 <124725617+Tata5773@users.noreply.github.com>
2026-05-20 00:00:10 +0200

    Fix project config

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const pino = require('pino-http');
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import pino from 'pino-http';
 
 dotenv.config();
 
